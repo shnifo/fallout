7bcb3f4bd8ac5fd8e1d974e73c7a78702203b264
shnifo <shnifo@gmail.com>
2026-02-17 11:29:05 -0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -97,11 +97,11 @@ export /* Note: ammo field uses "-" for no-ammo entries so indexes stay consiste
 
 export const ammoTable = [
     { type: "Small rounds", roll: "1d6+6", cost: 5 }, // crit = 2x omnirounds: +1 dmg and can be used as small or large rounds
-    { type: "E-cell", roll: "1d6+5", cost: 10 },  // crit = 2x omnicells: +1 dmg and can be used as E-cells or MF-cells
-    { type: "Grenades", roll: "1d6+1", cost: 0 }, // crit = 2x legendary grenades (gain legendary effect)
-    { type: "Fuel", roll: "1d6+2", cost: 20 }, // crit = 3x yield
+    { type: "E-cell", roll: "1d6+4", cost: 10 },  // crit = 2x omnicells: +1 dmg and can be used as E-cells or MF-cells
+    { type: "Grenades", roll: "1d6", cost: 0 }, // crit = 2x legendary grenades (gain legendary effect)
+    { type: "Fuel", roll: "1d6", cost: 20 }, // crit = 3x yield
     { type: "Large rounds", roll: "1d6+4", cost: 10 }, // crit = 2x omnirounds: +1 dmg and can be used as small or large rounds
-    { type: "MF-cell", roll: "1d6+3", cost: 15 } // crit = 2x omnicells: +1 dmg and can be used as E-cells or MF-cells
+    { type: "MF-cell", roll: "1d6+2", cost: 15 } // crit = 2x omnicells: +1 dmg and can be used as E-cells or MF-cells
 ];
 export const grenadeTable = [
     { max: 6, name: "Scrap Grenade", dmg: "11 X splash", cost: "15c" },
