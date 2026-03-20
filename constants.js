728c537d51c9c36c81750c2a1350ac3e7b846132
shnifo <shnifo@gmail.com>
2026-03-19 18:59:31 -0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -19,7 +19,7 @@ export const armorTable = [ /* crit = choose weapon type and gain random legenda
     { roll: 12, text: "Assault armor: +20 def, 160c" },
     { roll: 13, text: "Mutant armor: +22 def, 170c" },
     { roll: 14, text: "Fiber armor: +24 def, 180c" },
-    { roll: 15, text: "Dragon armor: +26 def, +190c" },
+    { roll: 15, text: "Dragon armor: +26 def, 190c" },
     { roll: 16, text: "Titan armor: +28 def, 200c" }
 ];
 
